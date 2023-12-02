ab364e4d8ddd2e412da7dbb413d9c466e679419b
Inclusit <camillefranzen@gmail.com>
2023-12-02 19:40:06 +0100

    Updates

--- src/Flag-application.jsx
+++ src/Flag-application.jsx
@@ -2,14 +2,14 @@ import { useEffect, useState } from "react";
 import Flags from "./Flags";
 
 export default function FlagApplication() {
-    const [continent, setContinent] = useState("");
+    const [region, setRegion] = useState("");
     const [countries, setCountries] = useState([]);
-    const [showFlag, setShowFlag] = useState(false)
+    const [showFlags, setShowFlags] = useState(false)
 
     useEffect(() => {
       const fetchCountries = async() => {
         try {
-            let response = await fetch(` https://restcountries.com/v3.1/region/${continent}`);
+            let response = await fetch(` https://restcountries.com/v3.1/region/${region}`);
             let flagData = await response.json();
             setCountries(flagData)
         } catch (error) {
@@ -17,22 +17,24 @@ export default function FlagApplication() {
         }
       };
 
-      if (continent) {
+      if (region) {
         fetchCountries();
       }
 
-    }, [continent])
+    }, [region])
     
     return (
         <>
             <div className="optionContainer" id="optionContainer" style={{display:"flex", gap:"10px", marginBottom:"10px"}}>
                 <fieldset className="radioContainer">
-                    <legend>Choose your continent</legend>
-                        <label><input type="radio" value="africa" id="anyContinent" checked={continent === "africa"} onChange={()=>setContinent("africa")}  />Afrika</label>
-                        <label><input type="radio" value="asia" id="anyContinent" checked={continent === "asia"} onChange={() => setContinent("asia")} />Asia</label>
-                        <label><input type="radio" value="europe" id="anyContinent" checked={continent === "europe"} onChange={() => setContinent("europe")} />Europe</label>
+                    <legend>Choose your region</legend>
+                        <label><input type="radio" value="africa" id="anyContinent" checked={region === "africa"} onChange={()=>setRegion("africa")}  />Afrika</label>
+                        <label><input type="radio" value="asia" id="anyContinent" checked={region === "asia"} onChange={() => setRegion("asia")} />Asia</label>
+                        <label><input type="radio" value="europe" id="anyContinent" checked={region === "europe"} onChange={() => setRegion("europe")} />Europe</label>
                 </fieldset>
-                <fieldset>
+
+                    
+{/*                 <fieldset>
                     <legend>Choose your country</legend>
                     <select>
                         <option value="">Choose your country</option>
@@ -40,10 +42,10 @@ export default function FlagApplication() {
                             <option value={country.name.common}>{country.name.common}</option>
                         ))}
                     </select>
-                </fieldset>
+                </fieldset> */}
             </div>
-            <button onClick={() => setShowFlag(!showFlag)}>Fetch ye flag, matey</button>
-            {showFlag && <Flags country={countries}/>}
+            <button onClick={() => setShowFlags(!showFlags)}>Fetch ye flags, matey</button>
+            <div><ul>{showFlags && <Flags countries={countries}/>}</ul></div>
 
         </>
     );
--- src/Flags.jsx
+++ src/Flags.jsx
@@ -1,11 +1,34 @@
-import { useState, useEffect } from "react";
+import { useState} from "react";
 
-export default function Flags({country}) {
-    const [flags, setFlags] = useState([]);
-    console.log(country.name)
+export default function Flags({countries}) {
+
+    const [countryInfo, setCountryInfo] = useState(null)
+    
     return (
         <>
             <h1>flags ahoy!</h1>
+            <div className="flag-container-wrapper">
+                <div className="flag-container" >{countries.map((country) => {
+                    return (
+                        <>
+                            <div >
+                              <p><div><img src={country.flags.png} alt={country.name.common} onClick={() => { setCountryInfo(country)
+                            }}/></div></p>
+                            {countryInfo === country && (
+                                <>
+                                    <p><span>{country.name.common}</span></p>
+                                    <p><span>Capital:</span> {country.capital[0]}</p>
+                                    <p><span>Population:</span> {country.population.toLocaleString()}</p>
+                                </>
+                            )}
+                            
+                            </div>
+                            
+                            
+                        </>
+                    )
+                })}</div>
+            </div>
         </>
     );
 };
